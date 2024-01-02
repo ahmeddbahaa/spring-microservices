72fa258981536785e894f2173ffa8be1977d8fd4
ahmeddbahaa <ahmedbahaa0026@gmail.com>
2024-01-02 22:19:00 +0200

    limits service

---
+++ limits-service/src/main/java/com/bahaa/microservices/limitsservice/bean/Limit.java
@@ -0,0 +1,27 @@
+package com.bahaa.microservices.limitsservice.bean;
+
+public class Limit {
+    private int minimum;
+    private int maximum;
+
+    public Limit(int minimum, int maximum) {
+        this.minimum = minimum;
+        this.maximum = maximum;
+    }
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+
+}
---
+++ limits-service/src/main/java/com/bahaa/microservices/limitsservice/configuration/Configuration.java
@@ -0,0 +1,27 @@
+package com.bahaa.microservices.limitsservice.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")
+public class Configuration {
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+
+    private int minimum;
+    private int maximum;
+}
