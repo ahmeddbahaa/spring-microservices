dcfdfc6a1c0ce4588d3342a76b0949098036af75
ahmeddbahaa <ahmedbahaa0026@gmail.com>
2024-01-07 23:45:44 +0200

    Api gateway filter for logging any incoming request Added resilience4j support for rate limtting, retry on failure, bulkhead

--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -1,9 +1,9 @@
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10001,'USD','INR',65,'');
+values(10001,'USD','EGY',55,'');
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10002,'EUR','INR',75,'');
+values(10002,'EUR','EGY',65,'');
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10003,'AUD','INR',25,'');
+values(10003,'KWD','EGY',125,'');
